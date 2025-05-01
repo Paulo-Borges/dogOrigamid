12d25541e19d7e57437617ac5c6235bd959dc56e
paulo-borges <borgespaulo72@yahoo.com.br>
2025-05-01 20:05:53 -0300

    feat:  Starting TokenPost

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import UserPost from "../api/endpoints/UserPost.jsx";
+import TokenPost from "./endpoints/TokenPost.jsx";
 
 const Api = () => {
   return (
     <div>
       <h2>USER POST</h2>
       <UserPost />
+      <br />
+      <h2>TOKEN POST</h2>
+      <TokenPost />
     </div>
   );
 };
