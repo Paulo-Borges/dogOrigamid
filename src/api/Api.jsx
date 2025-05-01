5ee6129fa0e1fd04beeaa673c275cdc711459fc4
paulo-borges <borgespaulo72@yahoo.com.br>
2025-05-01 20:36:07 -0300

    feat: Starting PhotoPost

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UserPost from "../api/endpoints/UserPost.jsx";
 import TokenPost from "./endpoints/TokenPost.jsx";
+import PhotoPost from "./endpoints/PhotoPost.jsx";
 
 const Api = () => {
   return (
@@ -10,6 +11,9 @@ const Api = () => {
       <br />
       <h2>TOKEN POST</h2>
       <TokenPost />
+      <br />
+      <h2>PHOTO POST</h2>
+      <PhotoPost />
     </div>
   );
 };
